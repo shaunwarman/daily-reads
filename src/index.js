f16986e2b4f9452969b1bd882d142dda5909617a
Shaun Warman <swarman@paypal.com>
2016-02-15 17:27:57 -0600

    Add cron job

--- src/index.js
+++ src/index.js
@@ -2,44 +2,49 @@ import cron from 'node-cron';
 import nodemailer from 'nodemailer';
 import wreck from 'wreck';
 
-const subreddits  = ['javascript', 'webdev', 'programming', 'node'];
+const subreddits  = ['puppies'];
 
-let articles = [];
-let count = 0;
+
 let transporter = nodemailer.createTransport('smtps://user%40gmail.com:pass@smtp.gmail.com');
 
-subreddits.forEach(function (sub) {
+cron.schedule('1-31 * *', function () {
     
-    const baseUrl = 'https://www.reddit.com/r/';
-    const query = '/top.json?sort=top&t=week&limit=5';
-    const url = baseUrl + sub + query;
+    console.log('Cron running!');
+    let articles = [];
+    let count = 0;
     
-    wreck.request('GET', url, {}, (error, response) => {
-        
-        wreck.read(response, null, (error, body) => {
-            
-            let data = JSON.parse(body.toString('utf8'));
-            
-            let mappedData = data.data.children.map((children) => {
-                if (children.data.subreddit === 'puppies') {
-                    return '<img style="width: 50%; height: 50%;" src=\"' + children.data.thumbnail + '\">' + '</img>';
-                } else {
-                    return '<a style="font-size: 18px" href=' +  children.data.url + '>' + children.data.title + '</a>';
+    subreddits.forEach(function (sub) {
+
+        const baseUrl = 'https://www.reddit.com/r/';
+        const query = '/top.json?sort=top&t=week&limit=25';
+        const url = baseUrl + sub + query;
+
+        wreck.request('GET', url, {}, (error, response) => {
+
+            wreck.read(response, null, (error, body) => {
+
+                let data = JSON.parse(body.toString('utf8'));
+
+                let mappedData = data.data.children.map((children) => {
+                    if (children.data.subreddit === 'puppies') {
+                        return '<a href=' + children.data.url + '><img style="width: 50%;" src=\"' + children.data.thumbnail + '\">' + '</img></a>';
+                    } else {
+                        return '<a style="font-size: 18px" href=' +  children.data.url + '>' + children.data.title + '</a>';
+                    }
+                })
+                .map((article) => {
+                    return '<p style="margin: 0 auto 40px auto">'+ article +'</p>';
+                });
+
+                articles = Array.concat(articles, mappedData);
+
+                count++;
+                if (count === subreddits.length) {
+                    sendMail(articles);
                 }
             });
-            
-            let htmlData = mappedData.map((article) => {
-                return '<p style="margin: 0 auto 40px auto">'+ article +'</p>';
-            });
-    
-            articles = Array.concat(articles, htmlData);
-            
-            count++;
-            if (count === subreddits.length) {
-                sendMail(articles);
-            }
         });
-    });
+    }); 
 });
 
 function sendMail(articles) {
