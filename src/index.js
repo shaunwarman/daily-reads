7df2574232d2d4c182dd170a5e2cc8df260765d1
Shaun Warman <swarman@paypal.com>
2016-02-17 19:26:29 -0600

    Refactor to use async await

--- src/index.js
+++ src/index.js
@@ -2,45 +2,44 @@ import cron from 'node-cron';
 import nodemailer from 'nodemailer';
 import wreck from 'wreck';
 
-const subreddits  = ['javascript', 'webdev', 'programming', 'node'];
+const subreddits  = ['node', 'javascript', 'programming', 'webdev'];
 
-let articles = [];
-let count = 0;
 let transporter = nodemailer.createTransport('smtps://user%40gmail.com:pass@smtp.gmail.com');
 
-subreddits.forEach(function (sub) {
-    
-    const baseUrl = 'https://www.reddit.com/r/';
-    const query = '/top.json?sort=top&t=week&limit=5';
-    const url = baseUrl + sub + query;
-    
-    wreck.request('GET', url, {}, (error, response) => {
-        
-        wreck.read(response, null, (error, body) => {
-            
-            let data = JSON.parse(body.toString('utf8'));
-            
-            let mappedData = data.data.children.map((children) => {
-                if (children.data.subreddit === 'puppies') {
-                    return '<img style="width: 50%; height: 50%;" src=\"' + children.data.thumbnail + '\">' + '</img>';
+//cron.schedule('* * * * 0-7', function () {
+
+console.log('Cron running!');
+let articles = [];
+let count = 0;
+
+function getArticle(url) {
+    return new Promise((resolve, reject) => {
+        wreck.request('GET', url, {}, (error, response) => {
+            if (error) { reject(error); }
+
+            wreck.read(response, null, (error, body) => {
+                if (error) {
+                    reject(error);
                 } else {
-                    return '<a style="font-size: 18px" href=' +  children.data.url + '>' + children.data.title + '</a>';
+                    resolve(body.toString('utf8'));
                 }
             });
-            
-            let htmlData = mappedData.map((article) => {
-                return '<p style="margin: 0 auto 40px auto">'+ article +'</p>';
-            });
-    
-            articles = Array.concat(articles, htmlData);
-            
-            count++;
-            if (count === subreddits.length) {
-                sendMail(articles);
-            }
         });
     });
-});
+}
+
+function getArticles(subreddits) {
+    return subreddits.map((subreddit) => {
+        return getArticle(getUrl(subreddit));
+    });
+}
+
+function getUrl(subreddit) {
+    const baseUrl = 'https://www.reddit.com/r/';
+    const query = '/top.json?sort=top&t=week&limit=5';
+
+    return baseUrl + subreddit + query;
+}
 
 function sendMail(articles) {
     let mailOptions = {
@@ -57,3 +56,29 @@ function sendMail(articles) {
         console.log('Message sent: ' + response.response);
     });
 };
+
+async function main(subreddits) {
+
+    try {
+
+        const articles = await getArticles(subreddits);
+
+        /**
+         * Filter json here { data: [{children}*5]}, {}, {}, {}
+         */
+
+        const data = await Promise.all(articles);
+
+        const htmlData = JSON.parse(data);
+
+        const html = htmlData.data.children.map((children) => {
+            return '<p style="margin: 0 auto 40px auto"><a style="font-size: 18px" href=' +  children.data.url + '>' + children.data.title + '</a></p>';
+        });
+
+        sendMail(html);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+main(subreddits);
